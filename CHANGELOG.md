7e8e13aa2cdec9cf1fa7cdff296a07c36f42a5b2
lucas <lvcasjm@gmail.com>
2020-10-23 12:25:30 +1030

    chore: implement number input from whppt/nuxt, update changelog to reflect changes

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+## v1.0.0-rc3
+### Fixes
+- Implement the new dedicated Number input within [@whppt/nuxt]() to solve Vuex mutation issues.
+
+## v1.0.0-rc2
+### Features
+- Further updates to documentation to help with installation.
+
+### Fixes
+- Fix issues  where Vue.use() would fail too properly install components. 
+
+## v1.0.0-rc1
+### Features 
+- Initial release candidate.
+- Split Content component.
+- Initial Documentation.
--- lib/components/SplitContent/Editor.vue
+++ lib/components/SplitContent/Editor.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
-    <whppt-text-input
+    <whppt-number-input
       :id="`${$options._scopeId}-split-content-width`"
       :value="selectedComponent.value.width"
       label="First column width"
       placeholder="12"
-      type="number"
       min="0"
       max="10"
       info="Sets the first column's width. Calculated in 12ths, e.g. 2 = 2/12, 5 = 5/12"
       @input="setSelectedComponentState({ value: $event, path: 'width' })"
     />
-    <whppt-text-input
+    <whppt-number-input
       :id="`${$options._scopeId}-split-content-gap`"
       :value="selectedComponent.value.gap"
       label="Gap width"
       placeholder="5"
-      type="number"
       min="0"
       max="5"
       info="Sets the width of the gap between the columns. Calculated in 12ths, e.g. 2 = 2/12, 5 = 5/12"
@@ -28,11 +26,11 @@
 <script>
 import { mapActions, mapState } from 'vuex';
 
-import WhpptTextInput from '@whppt/nuxt/lib/components/ui/Input.vue';
+import WhpptNumberInput from '@whppt/nuxt/lib/components/ui/NumberInput.vue';
 
 export default {
   name: 'SplitContentGapEditor',
-  components: { WhpptTextInput },
+  components: { WhpptNumberInput },
   computed: {
     ...mapState('whppt-nuxt/editor', ['selectedComponent']),
   },
