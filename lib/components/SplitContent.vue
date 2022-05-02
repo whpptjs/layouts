df7bde02fcddf47b999e9abf92bc361057528e19
Ben_G <ben@sveltestudios.com>
2022-05-02 13:54:21 +0930

    fix: formatting fixes

--- lib/components/SplitContent.vue
+++ lib/components/SplitContent.vue
@@ -1,39 +1,39 @@
 <template>
-<div :class="{container}">
-  <div
-    :class="{
-      'reverse-desktop': content.reverse.desktop,
-      'reverse-mobile': content.reverse.mobile,
-    }"
-  >
-    <!-- This comment is in place in the case that you are using purgecss to optimise bundle size in production environments.
+  <div :class="{ container }">
+    <div
+      :class="{
+        'reverse-desktop': content.reverse.desktop,
+        'reverse-mobile': content.reverse.mobile,
+      }"
+    >
+      <!-- This comment is in place in the case that you are using purgecss to optimise bundle size in production environments.
       See: https://tailwindcss.com/docs/optimizing-for-production
       PURGE CLASSES to be included for dynamic width:
       md:w-1/12 md:w-2/12 md:w-3/12 md:w-4/12 md:w-5/12 md:w-6/12 md:w-7/12 md:w-8/12 md:w-9/12 md:w-10/12 md:w-11/12 md:w-12/12
       w-1/12 w-2/12 w-3/12 w-4/12 w-5/12 w-6/12 w-7/12 w-8/12 w-9/12 w-10/12 w-11/12 w-12/12
     -->
-    <div v-split-content="content">
-      <span v-if="activeMenuItem" class="whppt-layouts__settings">Split Content Settings</span>
-      <div class="whppt-layouts__split-content">
-        <w-content
-          class="whppt-layouts__content whppt-layouts__content--left"
-          :class="`md:${leftColumnWidth}`"
-          :content-items="content.left"
-          :inSplitContent="true"
-          :container="false"
-          :whitelist="whitelist"
-        />
-        <div v-if="content.gap && content.gap !== '0'" class="whppt-layouts__gap" :class="gapWidth">&nbsp;</div>
-        <w-content
-          class="whppt-layouts__content whppt-layouts__content--right"
-          :class="`md:${rightColumnWidth}`"
-          :content-items="content.right"
-          :inSplitContent="true"
-          :container="false"
-          :whitelist="whitelist"
-        />
+      <div v-split-content="content">
+        <span v-if="activeMenuItem" class="whppt-layouts__settings">Split Content Settings</span>
+        <div class="whppt-layouts__split-content">
+          <w-content
+            class="whppt-layouts__content whppt-layouts__content--left"
+            :class="`md:${leftColumnWidth}`"
+            :content-items="content.left"
+            :in-split-content="true"
+            :container="false"
+            :whitelist="whitelist"
+          />
+          <div v-if="content.gap && content.gap !== '0'" class="whppt-layouts__gap" :class="gapWidth">&nbsp;</div>
+          <w-content
+            class="whppt-layouts__content whppt-layouts__content--right"
+            :class="`md:${rightColumnWidth}`"
+            :content-items="content.right"
+            :in-split-content="true"
+            :container="false"
+            :whitelist="whitelist"
+          />
+        </div>
       </div>
-    </div>
     </div>
   </div>
 </template>
@@ -52,7 +52,7 @@ export default {
       type: Array,
       default: () => [],
     },
-    container: { type: Boolean, default: false }
+    container: { type: Boolean, default: false },
   },
   data: () => ({
     paddingSizes: {
